77dc5589954a2c7087a014b7831af065908f7e9c
jamesboyd2008 <jamesboyd2008@gmail.com>
2017-01-13 12:33:23 -0500

    Reduce reducers hohoho :santa:

--- reducers.js
+++ reducers.js
@@ -6,6 +6,30 @@ const initialState = {
   todos: []
 }
 
+let todos = (state = [], action) => {
+  swtich (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo
+      })
+    default:
+      return state
+  }
+}
+
 let todoApp(state = initialState, action) {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
@@ -13,25 +37,9 @@ let todoApp(state = initialState, action) {
         visibilityFilter: action.filter
       })
     case ADD_TODO:
-      return Object.assign({}, state, {
-        todos: [
-          ...state.todos,
-          {
-            text: action.text,
-            completed: false
-          }
-        ]
-      })
     case TOGGLE_TODO:
       return Object.assign({}, state, {
-        todos: todos.map((todo, index) => {
-          if (index === action.index) {
-            return Object.assign({}, todo, {
-              completed: !todo.completed
-            })
-          }
-          return todo
-        })
+        todos: todos(state.todos, action)
       })
     default:
       return state
