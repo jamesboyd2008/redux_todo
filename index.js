7861f6111426b8a0ac21023f2eba99fec7f9329f
jamesboyd2008 <jamesboyd2008@gmail.com>
2017-01-14 20:10:50 -0500

    Add container components.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 
+import App from './components/App'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import React from 'react'
+import { render } from 'react-dom'
 import todoApp from './reducers'
 
 let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
